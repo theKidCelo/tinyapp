95813457d59bafc3aba3c2a038aa652ce130ccf3
Ramy Mohamed <ramy.opene@gmail.com>
2023-01-23 18:59:48 -0500

    Created new route

--- express_server.js
+++ express_server.js
@@ -21,6 +21,16 @@ app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
 
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+
+app.get("/urls/:id", (req, res) => {
+    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+    res.render("urls_show", templateVars);
+  });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
