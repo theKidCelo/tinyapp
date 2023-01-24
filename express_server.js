a58325122187eb7af5d8a95aff89303a9ce0515a
Ramy Mohamed <ramy.opene@gmail.com>
2023-01-24 14:30:09 -0500

    implemented a delet functionality and route

--- express_server.js
+++ express_server.js
@@ -50,6 +50,11 @@ app.get("/urls/:id", (req, res) => {
     res.send("Ok"); // Respond with 'Ok' (we will replace this)
   });
 
+  app.post("/urls/:id/delete", (req, res) => {
+    delete urlDatabase[req.params.id]
+    res.redirect("/urls");
+  });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
