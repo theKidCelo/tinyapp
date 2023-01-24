f3df92b76737ea0e21fbabf3f4650eba8b51d9bf
Ramy Mohamed <ramy.opene@gmail.com>
2023-01-24 13:45:31 -0500

    created new routes, first post route, and new ejs: urls_new

--- express_server.js
+++ express_server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {}
+
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,10 +30,20 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
 app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
     res.render("urls_show", templateVars);
   });
+
+  app.post("/urls", (req, res) => {
+    console.log(req.body); // Log the POST request body to the console
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
