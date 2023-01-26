babd3ef07c631ff5ece62afaec2e04cff4053ee7
Ramy Mohamed <ramy.opene@gmail.com>
2023-01-26 15:36:40 -0500

    Completed login/logout feature

--- express_server.js
+++ express_server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser')
 
 function generateRandomString() {}
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,18 +28,29 @@ app.get("/hello", (req, res) => {
   });
 
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
+    const templateVars = {
+        urls: urlDatabase,
+        username: req.cookies['username']
+      };
+
     res.render("urls_index", templateVars);
   });
 
 app.get("/u/:id", (req, res) => {
     // const longURL = ...
-    const longURL = urlDatabase[req.params.id]
-    res.redirect(longURL);
+    const templateVars = {
+        id: req.params.id,
+        longURL: urlDatabase[req.params.id],
+        username: req.cookies['username']
+      };
+    res.redirect(longURL, templateVars);
   });
 
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+    const templateVars = {
+        username: req.cookies['username']
+      }
+    res.render("urls_new", templateVars);
   });
 
 app.get("/urls/:id", (req, res) => {
@@ -63,9 +76,16 @@ app.get("/urls/:id", (req, res) => {
   });
 
   app.post("/login", (req, res) => {
+  
+    res.cookie("username", req.body.username);
     console.log(req.body)
-    res.cookie("username" , req.body.username)
+    
     res.redirect("/urls");
+  })
+
+  app.post("/logout", (req, res) => {
+    res.clearCookie("username")
+    res.redirect('/urls')
   })
 
 
